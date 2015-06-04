0c9bb1be4f77f7ee89a4cf0abf49719cbb3566df
Conner Alegre <Conner.Alegre@gmail.com>
2015-06-04 11:04:07 -0600

    Refactor mode and averageScore function

--- grader.js
+++ grader.js
@@ -15,12 +15,9 @@ module.exports = {
     return 'F';
   },
   averageScore: function(arr) {
-    var arrLen = arr.length;
-    var sum = 0;
-    for (var i = 0; i < arrLen; i++) {
-      sum += arr[i];
-    }
-    return Math.floor(sum / arrLen);
+    return Math.floor(arr.reduce(function(a, b) {
+      return a + b;
+    }) / arr.length);
   },
   median: function(a) {
     a.sort(function(a, b) {
@@ -37,28 +34,26 @@ module.exports = {
     var determine = [];
     var multiModal = [];
     var currNum = 0;
-    arr = arr.sort(function(a, b) {
+    var arr = arr.sort(function(a, b) {
       return a - b;
-    });
-    for (var i = 0; i < arr.length; i++) {
-      currNum = arr[i];
-      determine[i] = 0;
-      for (var y = 0; y < arr.length; y++) {
-        if (arr[y] === currNum) {
-          determine[i]++;
+    }).map(function(a, idx, ary) {
+      currNum = a;
+      determine[idx] = 0;
+      ary.forEach(function(b) {
+        if (b === currNum) {
+          determine[idx]++;
         }
-      }
-    }
-    var largest = Math.max.apply(Math, determine);
-    var found = 0;
-    for (var x = 0; x < determine.length; x++) {
-      if (determine[x] === largest) {
-        multiModal.push(arr[x]);
-      }
-    }
-    var finalAnswerRegis = multiModal.filter(function(item, pos) {
-      return multiModal.indexOf(item) == pos;
+      });
+      return a;
     });
-    return finalAnswerRegis;
+    var largest = Math.max.apply(Math, determine);
+    determine.forEach(function(a, idx) {
+      if (a === largest) {
+        multiModal.push(arr[idx]);
+      }
+    });
+    return multiModal.filter(function(item, pos, ary) {
+      return ary.indexOf(item) == pos;
+    });
   }
 };
